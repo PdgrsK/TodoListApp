3d2f5254e5501d4b9ab90fcd5507482093c694de
user <user@example.com>
2025-09-17 13:55:12 +0300

    Add TodoItem model, TodoListManager and basic display functionality

---
+++ TodoListApp/Models/TodoItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoListApp.Models
+{
+    internal class TodoItem
+    {
+        // Свойства класса
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+
+        // Конструктор для удобного создания задач
+        public TodoItem(int id, string description)
+        {
+            Id = id;
+            Description = description;
+            IsCompleted = false; // По умолчанию задача не выполнена
+        }
+
+        // Метод для отображения статуса задачи
+        public string GetStatusDisplay()
+        {
+            return IsCompleted ? "[x]" : "[ ]";
+        }
+
+    }
+}
+
+
--- TodoListApp/Program.cs
+++ TodoListApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using TodoListApp.Models;
+using TodoListApp.Managers;
+
+namespace TodoListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the To-Do List Application!");
+
+            // Создаем экземпляр менеджера списка дел
+            var todoManager = new TodoListManager();
+
+            // Теперь вызываем методы через экземпляр менеджера
+            todoManager.DisplayTodoList();
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+
+
